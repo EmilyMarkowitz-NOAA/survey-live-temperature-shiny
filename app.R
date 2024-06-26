5b6940487a3cf7f7b5c6dbf8f595ee02e5ae6e0c
MichaelSchram-NOAA-Affiliate <michael.schram@noaa.gov>
2024-06-26 13:21:14 -0700

    Formatting updates
    Began tweaking code format to help with familiarization/readability.

--- app.R
+++ app.R
@@ -1,12 +1,12 @@
 # source("data_dl.R") # Download Data
 
-# Support scripts --------------------------------------------------------------
+# Support scripts -------------------------------------------------------------
 source(here::here("files_support", "style.R")) 
 source(here::here("files_support", "functions.R"))
 # source(here::here("files_support", "data_dl.R"))
 source(here::here("files_support", "data.R"))
 
-## ui code parsed by tabName ---------------------------------------------------
+# UI scripts parsed by tabName ------------------------------------------------
 source(here::here("files_ui", "ui_surveymap.R"))
 source(here::here("files_ui", "ui_metadata.R"))
 source(here::here("files_ui", "ui_glossary.R"))
@@ -28,245 +28,343 @@ ui <- tagList(
 
 ## Title -----------------------------------------------------------------------
 
-  title = tags$head(tags$title(paste0("NOAA Fisheries ",title0," | NOAA Fisheries
-", subtitle0)), 
-                    tags$link(rel="shortcut icon", 
-                              href="https://www.noaa.gov/sites/all/themes/custom/noaa/favicon.ico", 
-                              type="image/vnd.microsoft.icon")), 
+  title = 
+    tags$head(
+      tags$title(
+        paste0("NOAA Fisheries ",title0," | NOAA Fisheries", subtitle0)
+      ), 
+      tags$link(
+        rel="shortcut icon", 
+        href="https://www.noaa.gov/sites/all/themes/custom/noaa/favicon.ico", 
+        type="image/vnd.microsoft.icon"
+      )
+    ), 
   
 ## Header -----------------------------------------------------------------------
   
-  header = dashboardHeader(title = 
-                             tags$a(href = 'https://www.fisheries.noaa.gov/',
-                                    tags$img(src="FISHERIES-Logo WEB ONLY.png", width = '90'), 
-                                    HTML(title0), 
-                                    style = paste0("text-align: right; 
-                                    color: #10497e; 
-                                    font-weight: bold; 
-                                    font-size: 20px;
-                                    font-family:'Arial Narrow';")
-                                    ), 
-                           titleWidth = nchar(title0)*17,
-
-                        #For login
-                           tags$li(class = "dropdown", 
-                                   style = paste0("padding: 8px;"),
-                                   shinyauthr::logoutUI("logout", class = "btn-primary", 
-                                                        style = "background-color: #1f93d0; border: none; color: #ffffff")),
-                           
-                           
-                           #Other Icons
-                            dropdownMenu(
-                             tags$li(tags$style(HTML('color: #10497e;}'))),                          
-                             type = "notifications",
-                             icon = icon("question-circle"),
-                             # icon = tags$a(icon("question-circle")#,
-                             #               # href = "https://github.com/emilyhmarkowitz/ShinyTemplateNMFS",
-                             #               # title = "Also see:",
-                             #               # style = "color: #1f93d0;"
-                             #               ),
-                             badgeStatus = NULL,
-                             headerText = "See also:",
-                             # style = "color: #1f93d0;")
-                             notificationItem("NOAA Fisheries", icon = icon("fish"), status = "info", # TOLEDO
-                                              href = "https://www.fisheries.noaa.gov/"),
-                             notificationItem("AFSC RACE Division", icon = icon("ship"), status = "info",
-                                              href = "https://www.fisheries.noaa.gov/about/resource-assessment-and-conservation-engineering-division")
-                           ),
-                           
-                           tags$li(class = "dropdown", 
-                                   tags$a(icon("github"), 
-                                          href = "https://github.com/EmilyMarkowitz-NOAA/AFSCRACE_SurveyDataMapApp",
-                                          title = "See the code on github", 
-                                          style = "color: #1f93d0;"))
-                           ), 
+  header = 
+    dashboardHeader(
+      title = 
+        tags$a(
+          href = 'https://www.fisheries.noaa.gov/',
+          tags$img(src="FISHERIES-Logo WEB ONLY.png", width = '90'), 
+          HTML(title0), 
+          style = 
+            paste0(
+              "text-align: right; 
+              color: #10497e; 
+              font-weight: bold; 
+              font-size: 20px;
+              font-family:'Arial Narrow';"
+            )
+        ), 
+      titleWidth = nchar(title0)*17,
+  
+      #For login
+      tags$li(
+        class = "dropdown", 
+        style = paste0("padding: 8px;"),
+        shinyauthr::logoutUI(
+          "logout", 
+          class = "btn-primary", 
+          style = "background-color: #1f93d0; border: none; color: #ffffff"
+        )
+      ),
+       
+       
+      #Other Icons
+      dropdownMenu(
+        tags$li(
+          tags$style(
+            HTML('color: #10497e;}')
+          )
+        ),                          
+        type = "notifications",
+        icon = icon("question-circle"),
+        # icon = tags$a(icon("question-circle")#,
+        #               # href = "https://github.com/emilyhmarkowitz/ShinyTemplateNMFS",
+        #               # title = "Also see:",
+        #               # style = "color: #1f93d0;"
+        #               ),
+        badgeStatus = NULL,
+        headerText = "See also:",
+        # style = "color: #1f93d0;")
+        notificationItem(
+         "NOAA Fisheries", 
+         icon = icon("fish"), 
+         status = "info", # TOLEDO
+         href = "https://www.fisheries.noaa.gov/"
+        ),
+        notificationItem(
+         "AFSC RACE Division", 
+         icon = icon("ship"), 
+         status = "info",
+         href = "https://www.fisheries.noaa.gov/about/resource-assessment-and-conservation-engineering-division"
+        )
+      ),
+     
+      tags$li(
+       class = "dropdown", 
+       tags$a(
+         icon("github"), 
+         href = "https://github.com/EmilyMarkowitz-NOAA/AFSCRACE_SurveyDataMapApp",
+         title = "See the code on github", 
+         style = "color: #1f93d0;"
+       )
+      )
+    ), 
   
 ## Sidebar -----------------------------------------------------------------------
-sidebar = dashboardSidebar(
-    collapsed = TRUE, 
-    width = nchar(title0)*17, 
-    
-    #Login
-    div(textOutput("welcome"), 
+  sidebar = 
+    dashboardSidebar(
+      collapsed = TRUE, 
+      width     = nchar(title0)*17, 
+      
+      #Login
+      div(textOutput("welcome"), 
         style = 
-        "padding-top: 40px; 
-         padding-bottom: 20px; 
+          "padding-top: 40px; 
+          padding-bottom: 20px; 
           text-align:center; 
-        color: #10497e; 
-        font-weight: bold; 
-        font-size: 20px;
-        font-family:'Arial Narrow';"), # for Login
-    
-    sidebarMenu(
-      id = "tabs",
-      menuItem("Survey Map", 
-               tabName = "surveymap", 
-               icon = icon("file-image")), # icon = icon("file-image-o")),
-      # menuItem(HTML(paste0("Welcome")),
-      #          tabName = "welcome", icon = icon("address-card")), #icon("sitemap")
-      menuItem(HTML(paste0("Metadata")),
-               tabName = "metadata", icon = icon("cogs")), #icon("sitemap")
-      menuItem(HTML(paste0("Glossary and Literature Cited")),
-               tabName = "glossary", icon = icon("road")), #icon("sitemap")
-      menuItem(HTML(paste0("Download Data")),
-               tabName = "data", icon = icon("road")), #icon("sitemap")
-      # menuItem("Import Data", 
-      #          tabName = "import", icon = icon("cloud-upload")),
-      # menuItem("Calculator", 
-      #          tabName = "calculator", icon = icon("cogs")),
-      menuItem("Licencing", 
-               tabName = "licencing", icon = icon("list-alt")),
-      menuItem("Manual", 
-               tabName = "manual", icon = icon("book"),
-               menuSubItem("Sub Menu Item 1", tabName = "sub_1"), 
-               menuSubItem("Sub Menu Item 2", tabName = "sub_2")
+          color: #10497e; 
+          font-weight: bold; 
+          font-size: 20px;
+          font-family:'Arial Narrow';"
+      ), 
+      
+      sidebarMenu(
+        id = "tabs",
+        menuItem(
+          "Survey Map", 
+          tabName = "surveymap", 
+          icon    = icon("file-image")
+        ),
+        # menuItem(
+        #   "Welcome",
+        #   tabName = "welcome", 
+        #   icon    = icon("address-card")
+        # ), 
+        menuItem(
+          "Metadata",
+          tabName = "metadata", 
+          icon    = icon("cogs")
+        ),
+        menuItem(
+          "Glossary and Literature Cited",
+          tabName = "glossary", 
+          icon    = icon("road")
+        ),
+        menuItem(
+          "Download Data",
+          tabName = "data", 
+          icon    = icon("road")
+        ),
+        # menuItem(
+        #   "Import Data", 
+        #   tabName = "import", 
+        #   icon    = icon("cloud-upload")
+        # ),
+        # menuItem(
+        #   "Calculator",
+        #   tabName = "calculator", 
+        #   icon    = icon("cogs")
+        # ),
+        menuItem(
+          "Licencing", 
+          tabName = "licencing", 
+          icon    = icon("list-alt")
+        ),
+        menuItem(
+          "Manual", 
+          tabName = "manual", 
+          icon    = icon("book"),
+          menuSubItem(
+            "Sub Menu Item 1", 
+            tabName = "sub_1"
+          ), 
+          menuSubItem(
+            "Sub Menu Item 2", 
+            tabName = "sub_2"
+          )
+        )
       )
-    )
-  ),
+    ),
   
 ## Body -----------------------------------------------------------------------
-body = dashboardBody(
-    
-    shinyjs::useShinyjs(),
-    tags$head(tags$style(HTML('
-      .main-header .sidebar-toggle:before {
-                              color: #10497e;}'))),
-    tags$head(tags$style(".table{margin: 0 auto;}"),
-              tags$script(src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.16/iframeResizer.contentWindow.min.js",
-                          type="text/javascript"),
-              # includeScript("www/returnClick.js"), 
-      tags$link(rel = "stylesheet", type = "text/css", href = "custom.css"),
-      tags$script(src = "./www/custom.js"),
-    # ),
-    
-    
-    # tags$head(
-    tags$style(HTML('
-    /* logo */
-      .skin-black .main-header .logo {
-        background-color: #ffffff;
-        height: 65px;
-      }
+  body = 
+    dashboardBody(
+      shinyjs::useShinyjs(),
+      tags$head(
+        tags$style(
+          HTML(
+            '.main-header .sidebar-toggle:before {color: #10497e;}'
+          )
+        )
+      ),
+      tags$head(
+        tags$style(
+          ".table{margin: 0 auto;}"
+        ),
+        tags$script(
+          src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.16/iframeResizer.contentWindow.min.js",
+          type="text/javascript"
+        ),
+        # includeScript("www/returnClick.js"), 
+        tags$link(
+          rel = "stylesheet", 
+          type = "text/css", 
+          href = "custom.css"
+        ),
+        tags$script(
+          src = "./www/custom.js"
+        ),
+      # ),
+      
+      # tags$head(
+        tags$style(
+          HTML('
+            /* logo */.skin-black 
+            .main-header 
+            .logo {
+              background-color: #ffffff; 
+              height: 65px;
+            }
+      
+            /* logo when hovered */
+            .skin-black 
+            .main-header 
+            .logo:hover {
+              background-color: #ffffff;
+              color: #000000;
+            }
+      
+            /* navbar (rest of the header) */
+            .skin-black 
+            .main-header 
+            .navbar {
+              background-image: linear-gradient(to right, #ffffff , #d9effa);
+              color: #000000;
+            }
+      
+            /* main sidebar */
+            .skin-black 
+            .main-sidebar {
+              background-color: #d9effa;
+            }
+      
+            /* active selected tab in the sidebarmenu */
+            .skin-black 
+            .main-sidebar 
+            .sidebar 
+            .sidebar-menu 
+            .active a{
+              background-color: #1f93d0;
+              color: #ffffff;
+            }
+      
+            /* other links in the sidebarmenu */
+            .skin-black 
+            .main-sidebar 
+            .sidebar 
+            .sidebar-menu a {
+              background-color: #d9effa;
+              color: #10497e;
+            }
+      
+            /* other links in the sidebarmenu when hovered */
+            .skin-black 
+            .main-sidebar 
+            .sidebar 
+            .sidebar-menu a:hover {
+              background-color: #1f93d0;
+              color: #ffffff;
+            }
+      
+            /* toggle button when hovered  */
+            .skin-black 
+            .main-header 
+            .navbar 
+            .sidebar-toggle:hover{
+              background-color: #1f93d0;
+              color: #10497e;
+            }
+      
+            /* body */
+            .content-wrapper, 
+            .right-side {
+              background-color: #ffffff;
+              color: #000000;
+            }
+      
+            .content-wrapper,
+            .right-side {
+              background-color: #ffffff;
+              color: #000000;
+              padding: 30px;
+            }
+      
+            .content-wrapper {
+              background-color: #ffffff !important;
+              color: #000000;
+             .leaflet-top
+             .leaflet-control {
+                margin: 0px;
+             }
+      
+            .leaflet-right {
+               margin-right: 40px;
+            }    
+            .full {
+              background-color: blue;
+              border-radius: 50%;
+              width: 20px;
+              height: 20px;
+              float: left;
+            }
 
-      /* logo when hovered */
-      .skin-black .main-header .logo:hover {
-        background-color: #ffffff;
-          color: #000000;
-      }
+            .circle {
+              background-color: #FFF;
+              border: 3px solid blue;
+              border-radius: 50%;
+              height: 20px;
+              width: 20px;
+            }
 
-      /* navbar (rest of the header) */
-      .skin-black .main-header .navbar {
-      background-image: linear-gradient(to right, #ffffff , #d9effa);
-          color: #000000;
-      }
-
-      /* main sidebar */
-      .skin-black .main-sidebar {
-        background-color: #d9effa;
-      }
-
-      /* active selected tab in the sidebarmenu */
-      .skin-black .main-sidebar .sidebar .sidebar-menu .active a{
-        background-color: #1f93d0;
-          color: #ffffff ;
-      }
-
-      /* other links in the sidebarmenu */
-      .skin-black .main-sidebar .sidebar .sidebar-menu a{
-        background-color: #d9effa;
-          color: #10497e;
-      }
-
-      /* other links in the sidebarmenu when hovered */
-      .skin-black .main-sidebar .sidebar .sidebar-menu a:hover{
-        background-color: #1f93d0;
-          color: #ffffff;
-      }
-
-      /* toggle button when hovered  */
-      .skin-black .main-header .navbar .sidebar-toggle:hover{
-        background-color: #1f93d0;
-          color: #10497e;
-      }
-
-      /* body */
-      .content-wrapper, .right-side {
-        background-color: #ffffff;
-          color: #000000;
-
-      }
-
-      .content-wrapper,
-      .right-side {
-      background-color: #ffffff;
-          color: #000000;
-      padding: 30px;
-      }
-
-     .content-wrapper {
-     background-color: #ffffff !important;
-          color: #000000;
-          
-          
-                               .leaflet-top .leaflet-control {
-                                   margin: 0px;
-                                }    
-
-                                .leaflet-right {
-                                     margin-right: 40px;
-                                  }    
-                                .full{
-                                background-color: blue;
-                                border-radius: 50%;
-                                width: 20px;
-                                height: 20px;
-                                float: left;
-
-                                }
-                                .circle {
-                                background-color: #FFF;
-                                border: 3px solid blue;
-                                border-radius: 50%;
-                                height: 20px;
-                                width: 20px;
-
-                                }
-
-                                .leaflet-control i{
-                                  margin-right: 25px;
-     }
-
-
-
-                                    '))),
-    # div {
-    #   padding-left: 5px;
-    #   padding-top: 10px;
-    # }
-    
-    
-    #Login
-    # shinyauthr::loginUI("login"),
-    # uiOutput("user_table"),
-    # HTML('<div data-iframe-height></div>'),
-    
-    tabItems(
-    #   tabItem(
-    #     tabName = "welcome"#, 
-    #     uiOutput("ui.welcome")
-    #     ),
-      ui.surveymap(),     # Welcome
-      # ui.welcome(),     # Welcome
-      ui.metadata(),      # Roadmap
-      ui.glossary(),      # Roadmap
-      # ui.plots(),        # High Quality Maps
-      ui.data(),        # High Quality Maps
-      # ui.import(),       # Import Data
-      # ui.calculator(),   # Evaluation Metrics
-      ui.licencing(),       # Export Predictions
-      ui.manual()        # Manual
+            .leaflet-control i{
+              margin-right: 25px;
+            }'
+          )
+        )
+      ),
+      # div {
+      #   padding-left: 5px;
+      #   padding-top: 10px;
+      # }
+      
+      
+      #Login
+      # shinyauthr::loginUI("login"),
+      # uiOutput("user_table"),
+      # HTML('<div data-iframe-height></div>'),
+      
+      tabItems(
+      #   tabItem(
+      #     tabName = "welcome"#, 
+      #     uiOutput("ui.welcome")
+      #     ),
+        ui.surveymap(),     # Welcome
+        # ui.welcome(),     # Welcome
+        ui.metadata(),      # Roadmap
+        ui.glossary(),      # Roadmap
+        # ui.plots(),        # High Quality Maps
+        ui.data(),        # High Quality Maps
+        # ui.import(),       # Import Data
+        # ui.calculator(),   # Evaluation Metrics
+        ui.licencing(),       # Export Predictions
+        ui.manual()        # Manual
+      )
     )
-  )), 
+  ), 
   
 ## Footer -----------------------------------------------------------------------
 
