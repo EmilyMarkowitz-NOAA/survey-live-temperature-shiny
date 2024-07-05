fb67d2706094bcc761e18cbc76c268e05d4b3983
MichaelSchram-NOAA-Affiliate <michael.schram@noaa.gov>
2024-07-05 09:45:40 -0700

    Module implementation
    Testing out module implementation

--- files_ui/ui_test.R
+++ files_ui/ui_test.R
@@ -1,89 +1,90 @@
-ui.test <- function() {
-  tabItem(
-    tabName = "test",
-    fluidPage(
-      leafletOutput(
-        "mymap",
+ui_test <- function(id) {
+  ns <- NS(id)
+  
+  fluidPage(
+    
+    leafletOutput(
+      ns("mymap"),
+    ),
+    
+    absolutePanel(
+      id        = "controls",
+      class     = "panel panel-default",
+      fixed     = TRUE,
+      draggable = TRUE,
+      top       = "550px",
+      left      = "auto",
+      right     = "15px",
+      bottom    = "auto",
+      width     = "auto",
+      height    = "auto",
+      
+      br(),
+      
+      selectInput(
+        inputId  = ns("year"), 
+        label    = "Year", 
+        choices  = sort(unique(dat$year)), 
+        # selected = max(dat$year), 
+        selected = 2023, # FOR TESTING
+        multiple = FALSE
       ),
-      absolutePanel(
-        id        = "controls",
-        class     = "panel panel-default",
-        fixed     = TRUE,
-        draggable = FALSE,
-        top       = "550px",
-        left      = "auto",
-        right     = "15px",
-        bottom    = "auto",
-        width     = "auto",
-        height    = "auto",
-        
-        br(),
-        
-        selectInput(
-          inputId  = "year", 
-          label    = "Year", 
-          choices  = sort(unique(dat$year)), 
-          # selected = max(dat$year), 
-          selected = 2023, # FOR TESTING
-          multiple = FALSE
+      
+      selectInput(
+        inputId = ns("plot_display"),
+        label   = "Display",
+        choices = c(
+          "Points" = "pt",
+          "Coldpool"    = "coldpool"
         ),
-        
-        selectInput(
-          inputId = "plot_display",
-          label   = "Display",
-          choices = c(
-            "Points" = "pt",
-            "Coldpool"    = "coldpool"
-          ),
-          selected = "pt"
-        ),
-        
-        selectInput(
-          inputId  = "survey", 
-          label    = "Survey", 
-          choices  = sort(
-            unique(
-              # Removes "NEBS" if present in the list
-              grep(
-                "NEBS",
-                shp_all$survey.area$SRVY,
-                invert = TRUE,
-                value  = TRUE
-              )
+        selected = "pt"
+      ),
+      
+      selectInput(
+        inputId  = ns("survey"), 
+        label    = "Survey", 
+        choices  = sort(
+          unique(
+            # Removes "NEBS" if present in the list
+            grep(
+              "NEBS",
+              shp_all$survey.area$SRVY,
+              invert = TRUE,
+              value  = TRUE
             )
-          ), 
-          selected = c("EBS", "NBS"), 
-          multiple = TRUE
-        ),
-        checkboxInput(
-          inputId = "station", 
-          label   = "Station Points", 
-          value   = FALSE
-        ),
-        checkboxInput(
-          inputId = "stratum", 
-          label   = "Stratum", 
-          value   = FALSE
+          )
         ), 
-        checkboxInput(
-          inputId = "vessel", 
-          label   = "Vessels", 
-          value   = FALSE
-        ), 
-        
-        br(),
-        
-        selectInput(
-          inputId = "plot_unit",
-          label   = "Environmental Variable",
-          choices = c(
-            "Bottom Temperature (°C)" = "bottom_temperature_c",
-            "Surface Temperature (°C)" = "surface_temperature_c",
-            "None" = "none"
-          ),
-          selected = "none",
+        selected = c("EBS", "NBS"), 
+        multiple = TRUE
+      ),
+      checkboxInput(
+        inputId = ns("station"), 
+        label   = "Station Points", 
+        value   = FALSE
+      ),
+      checkboxInput(
+        inputId = ns("stratum"), 
+        label   = "Stratum", 
+        value   = FALSE
+      ), 
+      checkboxInput(
+        inputId = ns("vessel"), 
+        label   = "Vessels", 
+        value   = FALSE
+      ), 
+      
+      br(),
+      
+      selectInput(
+        inputId = ns("plot_unit"),
+        label   = "Environmental Variable",
+        choices = c(
+          "Bottom Temperature (°C)" = "bottom_temperature_c",
+          "Surface Temperature (°C)" = "surface_temperature_c",
+          "None" = "none"
         ),
-      )
+        selected = "none",
+      ),
     )
   )
 }
