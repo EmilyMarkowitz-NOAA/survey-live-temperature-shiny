9b8ed84c7ad2e0bcc66ab67a285f8a4f5cded54b
MichaelSchram-NOAA-Affiliate <michael.schram@noaa.gov>
2024-07-05 09:44:15 -0700

    Update app.R
    Major formatting and organizational updates to confirm with Shiny modules.
    
    Also removed unused code chunks to clean up script - may reintroduce those chunks later

--- app.R
+++ app.R
@@ -1,12 +1,11 @@
-# source("data_dl.R") # Download Data
-
-# Support scripts --------------------------------------------------------------
+# Source scripts ---------------------------------------------------------------
+## Support scripts -------------------------------------------------------------
 source(here::here("files_support", "style.R")) 
 source(here::here("files_support", "functions.R"))
 # source(here::here("files_support", "data_dl.R"))
 source(here::here("files_support", "data.R"))
 
-# UI scripts parsed by tabName -------------------------------------------------
+## UI scripts parsed by feature ------------------------------------------------
 source(here::here("files_ui", "ui_surveymap.R"))
 source(here::here("files_ui", "ui_metadata.R"))
 source(here::here("files_ui", "ui_glossary.R"))
@@ -16,19 +15,20 @@ source(here::here("files_ui", "ui_data.R"))
 source(here::here("files_ui", "ui_licencing.R"))
 source(here::here("files_ui", "ui_manual.R"))
 
-# Server script parsed by feature ----------------------------------------------
-# readline(here::here("files_server", "s_surveymap.R"))
+## Server script parsed by feature ---------------------------------------------
 source(here::here("files_server", "s_test.R"))
 source(here::here("files_server", "s_surveymap.R"))
 # source(here::here("files_server", "s_glossary.R"))
 # source(here::here("files_server", "s_data.R"))
 # source(here::here("files_server", "s_glossary.R"))
 
-# Define -----------------------------------------------------------------------
+# User Interface ---------------------------------------------------------------
+## Define -----------------------------------------------------------------------
 title0 <- " | Bottom Trawl Survey Temperature and Progress Maps "
 subtitle0 <-  "NOAA Fisheries scientists share information on ocean temperatures recorded during the Aleutian Islands, Gulf of Alaska and Bering Sea Bottom Trawl Surveys"
 
-# ## Header ----------------------------------------------------------------------
+
+## Header ----------------------------------------------------------------------
 header <-
   shinydashboardPlus::dashboardHeader(
     title =
@@ -94,52 +94,61 @@ sidebar =
     
     ### Sidebar menu items -----------------------------------------------------
     sidebarMenu(
-      id = "tabs",
+      # Bottom trawl survey progress map(s)
       menuItem(
         "Survey Map", 
         tabName = "surveymap", 
         icon    = icon("file-image")
       ),
+      # Bottom trawl survey progress map(s) TEST
       menuItem(
         "Test", 
         tabName = "test", 
         icon    = icon("globe")
       ),
-      # menuItem(
-      #   "Welcome",
-      #   tabName = "welcome",
-      #   icon    = icon("address-card")
-      # ),
+      # Welcome
+      menuItem(
+        "Welcome",
+        tabName = "welcome",
+        icon    = icon("address-card")
+      ),
+      # Metadata
       menuItem(
         "Metadata",
-        tabName = "metadata", 
+        tabName = "metadata",
         icon    = icon("cogs")
       ),
+      # Glossary of Terms
       menuItem(
         "Glossary and Literature Cited",
-        tabName = "glossary", 
+        tabName = "glossary",
         icon    = icon("road")
       ),
+      # Access to the QAQC data
       menuItem(
         "Download Data",
-        tabName = "data", 
+        tabName = "data",
         icon    = icon("road")
       ),
-      # menuItem(
-      #   "Import Data", 
-      #   tabName = "import", 
-      #   icon    = icon("cloud-upload")
-      # ),
-      # menuItem(
-      #   "Calculator",
-      #   tabName = "calculator", 
-      #   icon    = icon("cogs")
-      # ),
+      # Import Data
       menuItem(
-        "Licencing", 
-        tabName = "licencing", 
+        "Import Data",
+        tabName = "import",
+        icon    = icon("cloud-upload")
+      ),
+      # Evaluation Metrics
+      menuItem(
+        "Calculator",
+        tabName = "calculator",
+        icon    = icon("cogs")
+      ),
+      # Export Predictions
+      menuItem(
+        "Licencing",
+        tabName = "licencing",
         icon    = icon("list-alt")
       ),
+      # Manual
       menuItem(
         "Manual", 
         tabName = "manual", 
@@ -159,52 +168,66 @@ sidebar =
 ## Body ----------------------------------------------------------------------
 body <-  
   dashboardBody(
-    # shinyjs::useShinyjs(),
-    # tags$head(
-    #   tags$style(
-    #     HTML(
-    #       '.main-header
-    #       .sidebar-toggle:before {
-    #         color: #10497e;
-    #       }'
-    #     )
-    #   )
-    # ),
-    # 
-    # tags$head(
-    #   tags$style(
-    #     ".table{margin: 0 auto;}"
-    #   ),
-    #   tags$script(
-    #     src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.16/iframeResizer.contentWindow.min.js",
-    #     type="text/javascript"
-    #   ),
-    #   tags$link(
-    #     rel = "stylesheet",
-    #     type = "text/css",
-    #     href = "custom.css"
-    #   ),
-    #   tags$script(
-    #     src = "./www/custom.js"
-    #   ),
-    # ),
-
     tabItems(
-      ui.surveymap(),     # Bottom trawl survey progress map(s)
-      # ui.welcome(),       # Welcome
-      ui.metadata(),      # Roadmap
-      ui.test(),          # TEST TEST TEST
-      ui.glossary(),      # Glossary of Terms
-      # ui.plots(),       # High Quality Maps
-      ui.data(),          # Access to the QAQC data
-      # ui.import(),      # Import Data
-      # ui.calculator(),  # Evaluation Metrics
-      ui.licencing(),     # Export Predictions
-      ui.manual()         # Manual
+      # Bottom trawl survey progress map(s)
+      # tabItem(
+      #   tabName = "surveymap",
+      #   ui.surveymap("map1")
+      # ),
+      # Bottom trawl survey progress map(s) TEST
+      tabItem(
+        tabName = "test", 
+        ui_test("maptest")           
+      )
+      # # Welcome
+      # tabItem(
+      #   tabName = "",
+      #   ui.welcome()
+      # ),
+      # # Metadata
+      # tabItem(
+      #   tabName = "",
+      #   ui.metadata()
+      # ),
+      # # Glossary of Terms
+      # tabItem(
+      #   tabName = "",
+      #   ui.glossary()            
+      # ),
+      # # High Quality Maps
+      # tabItem(
+      #   tabName = "",
+      #   ui.plots()          
+      # ),
+      # # Access to the QAQC data
+      # tabItem(
+      #   tabName = "",
+      #   ui.data()          
+      # ),
+      # # Import Data
+      # tabItem(
+      #   tabName = "",
+      #   ui.import()          
+      # ),
+      # # Evaluation Metrics
+      # tabItem(
+      #   tabName = "",
+      #   # ui.calculator()         
+      # ),
+      # # Export Predictions
+      # tabItem(
+      #   tabName = "",
+      #   ui.licencing()           
+      # ),
+      # # Manual
+      # tabItem(
+      #   tabName = "",
+      #   ui.manual()          
+      # )
     )
   )
 
-# User Interface - Dashboard ---------------------------------------------------
+## Call ---------------------------------------------------
 ui <- 
   dashboardPage(
     header, 
@@ -213,113 +236,8 @@ ui <-
   )
 
 # Server -----------------------------------------------------------------------
-#   
-#   ## Body ----------------------------------------------------------------------
-# 
-#   
-#   ## CSV Download --------------------------------------------------------------
-#   output$downloadData <- 
-#     downloadHandler(
-#     # filename <- paste0("NOAAAcousticThresholds_", Sys.Date(), ".csv"),
-#     filename = #function() {
-#       "downloadData.csv",
-#     # },
-#     contentType = "text/csv",
-#     content = 
-#       function(file) {
-#         filename0 <- file #"downloadData.csv"#here::here(getwd(), "downloadData.csv")
-#         
-#         # Threshold Isopleths Results WARNINGS
-#         write.table(
-#           input$dataset,
-#           file      = filename0,
-#           sep       = ",", 
-#           row.names = FALSE, 
-#           col.names = FALSE, 
-#           append    = TRUE
-#         )
-#         
-#         write.table(
-#           "Data",
-#           file      = filename0,
-#           sep       = ",", 
-#           row.names = FALSE, 
-#           col.names = FALSE, 
-#           append    = TRUE
-#         )
-#         
-#         write.table(
-#           input$datasetInput,
-#           file      = filename0,
-#           sep       = ",", 
-#           row.names = TRUE, 
-#           col.names = FALSE, 
-#           append    = TRUE
-#         )
-#         
-#         write.table(
-#           "", #Space
-#           file      = filename0,
-#           sep       = ",", 
-#           row.names = FALSE, 
-#           col.names = FALSE, 
-#           append    = TRUE
-#         )
-#         
-#         # DISCLAIMER
-#         write.table(
-#           "LICENCE",
-#           file      = filename0,
-#           sep       = ",", 
-#           row.names = FALSE, 
-#           col.names = FALSE, 
-#           append    = TRUE
-#         )
-#         
-#         write.table(
-#           licence0,
-#           file      =filename0,
-#           sep       = ",", 
-#           row.names = TRUE, 
-#           col.names = FALSE, 
-#           append    = TRUE
-#        )
-#       }
-#   )
-# 
-#   ## R Markdown Report  --------------------------------------------------------
-#   
-#   output$report <- 
-#     downloadHandler(
-#     # For PDF output, change this to "report.pdf"
-#     filename    = "report.html",
-#     contentType = "text/html",
-# 
-#     content = function(file) {
-#       # Copy the report file to a temporary directory before processing it, in
-#       # case we don't have write permissions to the current working dir (which
-#       # can happen when deployed).
-#       tempReport <- here::here(getwd(), "report4.Rmd")
-#       file.copy(from = "report4.Rmd", "report2.Rmd", overwrite = TRUE)
-#       file.copy("report2.Rmd", tempReport, overwrite = TRUE)
-#       
-#       # Set up parameters to pass to Rmd document
-#       params <- list(
-#         ProjectName = input$ProjectName,
-#         distPlot = input$distPlot,
-#         table = input$table
-#       )
-#       # Knit the document, passing in the `params` list, and eval it in a
-#       # child of the global environment (this isolates the code in the document
-#       # from the code in this app).
-#       rmarkdown::render(tempReport, output_file = file,
-#                         params = params,
-#                         envir = new.env(parent = globalenv())
-#       )
-#     }
-# 
-# 
-#   )
-# }
+server <- function(input, output, session){
+  s_test("maptest")
+}
 
 shinyApp(ui, server)
