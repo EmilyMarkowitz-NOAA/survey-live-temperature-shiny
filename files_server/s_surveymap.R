52ec6359bf36d1b9b63622b910ba96c351213abe
MichaelSchram-NOAA-Affiliate <michael.schram@noaa.gov>
2024-07-24 14:13:19 -0700

    Update s_surveymap.R
    Incorporated reactive, observe, and observeEvent (Issue #16) to improve efficiency.
    
    Also changed to leafletProxy to substantially increase update time after UI selection changes.

--- files_server/s_surveymap.R
+++ files_server/s_surveymap.R
@@ -18,33 +18,28 @@ s_surveymap <- function(id) {
         na.color = "transparent"
       )
     
-    # Reactive Expressions -------
-    ##
-    ## Temperature color palette ------
+    # Reactive Expressions -----
+    ## Temperature color palette -----
     pal_tmp <- reactive({
       leaflet::colorFactor(
         palette  = viridis_pal(
           begin  = 0.2,
           end    = 0.8,
           option = input$plot_color
-        )(length(unique(dat$bot_bin))),
-        domain = unique(dat$bot_bin),
-        na.color = viridis(
-          n      = 1,
-          begin  = 0.8,
-          end    = 0.8,
-          option = input$plot_color
-        )
+        )(length(unique(filter(dat, temperature_type == input$plot_unit)$temperature_bin))),
+        domain = unique(filter(dat, temperature_type == input$plot_unit)$temperature_bin),
+        na.color   = "transparent"
       )
     })
     
-    ## Subset data based on user selection ------
+    ## Subset data based on user selection -----
     dat_temps_grid <- reactive({
       dat %>%
       dplyr::filter(
         SRVY %in% input$survey,
-        SRVY %!in% c("AI", "GOA"),
         year == input$year,
+        temperature_type == input$plot_unit,
+        grepl("-", station),
         is.na(comment)
       ) %>%
       st_transform(crs = "+proj=longlat +datum=WGS84")
@@ -54,14 +49,15 @@ s_surveymap <- function(id) {
       dat %>%
         dplyr::filter(
           SRVY %in% input$survey,
-          SRVY %!in% c("AI", "GOA"),
           year == input$year,
+          temperature_type == input$plot_unit,
+          !grepl("-", station),
           !is.na(comment)
         ) %>%
         st_transform(crs = "+proj=longlat +datum=WGS84")
     }) 
     
-    # Build the map
+    # Build the base map -----
     output$mymap <- renderLeaflet({
       a <- 
         leaflet(
@@ -85,18 +81,18 @@ s_surveymap <- function(id) {
         addScaleBar(
           position = "bottomright"
         ) %>%
-        # Land mass polygons (i.e., Alaska)
+        ## Land mass polygons (i.e., Alaska) ----
         addPolygons(
-          # # AKFG shape files
-          # data = 
-          #   shp_all$akland %>%
-          #   st_transform(crs = "+proj=longlat +datum=WGS84"),
-          data = rnaturalearth::ne_countries(
-            country     = c("United States of America", "Canada", "Russia"),
-            scale       = "medium",
-            returnclass = "sf"
-          ) %>%
-          st_transform(crs = "+proj=longlat +datum=WGS84"),
+          # AKFG shape files
+          data =
+            shp_all$akland %>%
+            st_transform(crs = "+proj=longlat +datum=WGS84"),
+          # data = rnaturalearth::ne_countries(
+          #   country     = c("United States of America", "Canada", "Russia"),
+          #   scale       = "medium",
+          #   returnclass = "sf"
+          # ) %>%
+          # st_transform(crs = "+proj=longlat +datum=WGS84"),
           weight       = 0.5,
           color        = "black",
           opacity      = 0.5,
@@ -105,13 +101,14 @@ s_surveymap <- function(id) {
           label        = ~paste(name),
           labelOptions = labelOptions(direction = "auto")
         ) %>%
-        # Survey region polygons
+        ## Survey region defaults ----
         addPolygons(
           data = shp_all$survey.area %>%
             st_transform(crs = "+proj=longlat +datum=WGS84") %>%
             dplyr::filter(
-              SRVY %in% input$survey
+              SRVY %in% c("EBS", "NBS")
             ),
+          group          = "regions",
           weight         = 1,
           color          = "#444444",
           opacity        = 1,
@@ -120,15 +117,17 @@ s_surveymap <- function(id) {
           smoothFactor   = 0.5,
           label          = ~paste(survey_long),
           labelOptions   = labelOptions(direction = "auto")
-        )  %>%
+        ) %>%
+        ## Add mouse over coordinates ----
         addMouseCoordinates(
           epsg = "EPSG:3338"
-        ) %>% 
+        ) %>%
+        ## Add scale bars -----
         addMeasure(
           primaryLengthUnit = "kilometers",
           secondaryAreaUnit = "miles"
         # ) %>%
-        # # Add map graticules
+        # ## Add map graticules -----
         # addPolylines(
         #   data = shp_all$graticule %>%
         #     st_transform(crs = "+proj=longlat +datum=WGS84"),
@@ -137,7 +136,7 @@ s_surveymap <- function(id) {
         #   label  = ~paste(degree),
         #   labelOptions = labelOptions(direction = "auto")
         # ) %>%
-        # # Add bathymetric countours
+        # ## Add bathymetric countours ------
         # addPolylines(
         #   data = shp_all$bathymetry %>%
         #     st_transform(crs = "+proj=longlat +datum=WGS84") %>%
@@ -148,224 +147,170 @@ s_surveymap <- function(id) {
         #   color  = "#000000",
         #   label  = ~paste(meters),
         #   labelOptions = labelOptions(direction = "auto")
-        # ) %>%
-        # addDrawToolbar(
-        #   targetGroup ='draw',
-        #   editOptions = editToolbarOptions(
-        #     selectedPathOptions = selectedPathOptions()
-        #   ),
-        #   polylineOptions = filterNULL(
-        #     list(
-        #       shapeOptions = drawShapeOptions(
-        #         lineJoin = "round",
-        #         weight   = 3
-        #       )
-        #     )
-        #   ),
-        #   circleOptions = filterNULL(
-        #     list(
-        #       shapeOptions = drawShapeOptions(),
-        #       repeatMode   = FALSE,
-        #       showRadius   = TRUE,
-        #       metric       = TRUE,
-        #       feet         = FALSE,
-        #       nautic       = FALSE
-        #     )
-        #   )
+        ) %>%
+        ## Add drawing tools for measurements -----
+        addDrawToolbar(
+          targetGroup ='draw',
+          editOptions = editToolbarOptions(
+            selectedPathOptions = selectedPathOptions()
+          ),
+          polylineOptions = filterNULL(
+            list(
+              shapeOptions = drawShapeOptions(
+                lineJoin = "round",
+                weight   = 3
+              )
+            )
+          ),
+          circleOptions = filterNULL(
+            list(
+              shapeOptions = drawShapeOptions(),
+              repeatMode   = FALSE,
+              showRadius   = TRUE,
+              metric       = TRUE,
+              feet         = FALSE,
+              nautic       = FALSE
+            )
+          )
         )
-
+    })
+      
+    # Add Map features ----
+    observeEvent(input$updateButton, {
+      # Survey region polygons
+      leafletProxy(
+        "mymap"
+        ) %>%
+        clearGroup(
+          "regions"
+        ) %>%
+        addPolygons(
+        data = shp_all$survey.area %>%
+          st_transform(crs = "+proj=longlat +datum=WGS84") %>%
+          dplyr::filter(
+            SRVY %in% input$survey
+          ),
+        group          = "regions",
+        weight         = 1,
+        color          = "#444444",
+        opacity        = 1,
+        fillColor      = ~pal_shp(SRVY),
+        fillOpacity    = 0.2,
+        smoothFactor   = 0.5,
+        label          = ~paste(survey_long),
+        labelOptions   = labelOptions(direction = "auto")
+      )
+      
+      ## Temperature Data -----
       if (input$plot_unit != "none") {
+      
+        pal <- pal_tmp()
+        
         leafletProxy(
           "mymap"
         ) %>%
+          clearGroup(
+            c(
+              "temps_grid", 
+              "temps_crnr"
+            )
+          ) %>%
+          removeControl(
+            "temps_legend"
+          ) %>%
           addMapPane(
             "grid",
-            zIndex = 420
+            zIndex = 450
           ) %>%
           addMapPane(
             "corners",
-            zIndex = 420
+            zIndex = 460
           ) %>%
           addPolygons(
             data        = dat_temps_grid(),
+            group       = "temps_grid",
             options     = pathOptions(pane = "grid"),
             weight      = 1,
             color       = "black",
-            fillColor   = ~pal_tmp()(dat_temps_grid()$bot_bin),
-            fillOpacity = 1,
+            fillColor   = ~pal(temperature_bin),
+            fillOpacity = 0.9,
             popup       = paste(
-              "Survey:",
+              "<strong>Region:</strong>",
               dat_temps_grid()$survey_long,
               "<br>",
-              "Station:",
+              "<strong>Station:</strong>",
               dat_temps_grid()$station,
               "<br>",
-              "Temperature:",
-              dat_temps_grid()$bottom_temperature_c,
+              "<strong>Depth:</strong>",
+              dat_temps_grid()$depth_m,
+              "(m)",
+              "<br>",
+              "<strong>Temperature:</strong>",
+              dat_temps_grid()$temperature_c,
               "(°C)"
             )
           ) %>%
           addPolygons(
             data        = dat_temps_crnr(),
+            group       = "temps_crnr",
             options     = pathOptions(pane = "corners"),
             weight      = 1,
             color       = "black",
-            fillColor   = ~pal_tmp()(dat_temps_grid()$bot_bin),
-            fillOpacity = 1,
-            popup       = paste(
-              "Survey:",
-              dat_temps_crnr()$survey_long,
-              "<br>",
-              "Station:",
-              dat_temps_crnr()$station,
-              "<br>",
-              "Temperature:",
-              dat_temps_crnr()$bottom_temperature_c,
-              "(°C)"
-            ),
+            fillColor   = ~pal(temperature_bin),
+            fillOpacity = 0.9,
+          popup       = paste(
+            "<strong>Region:</strong>",
+            dat_temps_crnr()$survey_long,
+            "<br>",
+            "<strong>Station:</strong>",
+            dat_temps_crnr()$station,
+            "<br>",
+            "<strong>Depth:</strong>",
+            dat_temps_crnr()$depth_m,
+            "(m)",
+            "<br>",
+            "<strong>Temperature:</strong>",
+            dat_temps_crnr()$temperature_c,
+            "(°C)"
+            )
           ) %>%
           addLegend(
             position = "bottomright",
-            pal      = pal_tmp(),
-            values   = dat_temps_grid()$bot_bin,
+            layerId  = "temps_legend",
+            pal      = pal,
+            values   = dat_temps_grid()$temperature_bin,
             title    = "Temperature (°C)",
-            opacity  = 1.0
+            opacity  = 0.9
+          )
+      } else if (input$plot_unit == "none") {
+        leafletProxy(
+          "mymap"
+        ) %>%
+          clearGroup(
+            c(
+              "temps_grid", 
+              "temps_crnr"
+            )
+          ) %>%
+          removeControl(
+            "temps_legend"
           )
       }
-          # addPolygons(
-          #   data        = dat_temps_grid,
-          #   options     = pathOptions(pane = "grid"),
-          #   weight      = 1,
-          #   color       = "black",
-          #   fillColor   = ~pal_tmp(bot_bin),
-          #   fillOpacity = 1,
-          #   popup       = paste(
-          #     "Survey:",
-          #     dat_temps_grid$survey_long,
-          #     "<br>",
-          #     "Station:",
-          #     dat_temps_grid$station,
-          #     "<br>",
-          #     "Temperature:",
-          #     dat_temps_grid$bottom_temperature_c,
-          #     "(°C)"
-          #   ),
-          # ) %>%
-          # addPolygons(
-          #   data        = dat_temps_crnr,
-          #   options     = pathOptions(pane = "corners"),
-          #   weight      = 1,
-          #   color       = "black",
-          #   fillColor   = ~pal_tmp(bot_bin),
-          #   fillOpacity = 1,
-          #   popup       = paste(
-          #     "Survey:",
-          #     dat_temps_crnr$survey_long,
-          #     "<br>",
-          #     "Station:",
-          #     dat_temps_crnr$station,
-          #     "<br>",
-          #     "Temperature:",
-          #     dat_temps_crnr$bottom_temperature_c,
-          #     "(°C)"
-          #   ),
-          # )
-        
-      #   if (input$plot_unit == "bottom_temperature_c") {
-      #     a <- 
-      #       a %>%
-      #       addMapPane(
-      #         "grid", 
-      #         zIndex = 420
-      #       ) %>%
-      #       addMapPane(
-      #         "corners",
-      #         zIndex = 440
-      #       ) %>%
-      #       addPolygons(
-      #         data        = dat_temps_grid,
-      #         options     = pathOptions(pane = "grid"),
-      #         weight      = 1,
-      #         color       = "black",
-      #         fillColor   = ~pal_tmp(bot_bin),
-      #         fillOpacity = 1,
-      #         popup       = paste(
-      #           "Survey:",
-      #           dat_temps_grid$survey_long,
-      #           "<br>",
-      #           "Station:",
-      #           dat_temps_grid$station,
-      #           "<br>",
-      #           "Temperature:",
-      #           dat_temps_grid$bottom_temperature_c,
-      #           "(°C)"
-      #         ),
-      #       ) %>%
-      #       addPolygons(
-      #         data        = dat_temps_crnr,
-      #         options     = pathOptions(pane = "corners"),
-      #         weight      = 1,
-      #         color       = "black",
-      #         fillColor   = ~pal_tmp(bot_bin),
-      #         fillOpacity = 1,
-      #         popup       = paste(
-      #           "Survey:",
-      #           dat_temps_crnr$survey_long,
-      #           "<br>",
-      #           "Station:",
-      #           dat_temps_crnr$station,
-      #           "<br>",
-      #           "Temperature:",
-      #           dat_temps_crnr$bottom_temperature_c,
-      #           "(°C)"
-      #         ),
-      #       ) %>%
-      #       addLegend(
-      #         position = "bottomright",
-      #         pal      = pal_tmp,
-      #         values   = dat_temps$bot_bin,
-      #         title    = "Bottom </br> Temperature (°C)",
-      #         opacity  = 1.0
-      #       )
-      #   # } else if (input$plot_unit == "surface_temperature_c") {
-      #   #   a <- 
-      #   #     a %>%
-      #   #     addPolygons(
-      #   #       data        = dat_temps,
-      #   #       weight      = 1,
-      #   #       color       = "black", 
-      #   #       fillColor   = ~pal_tmp(sur_bin),
-      #   #       fillOpacity = 1,
-      #   #       popup       = paste(
-      #   #         "Survey:",
-      #   #         dat_temps$survey_long,
-      #   #         "<br>",
-      #   #         "Station:",
-      #   #         dat_temps$station,
-      #   #         "<br>",
-      #   #         "Temperature:",
-      #   #         dat_temps$surface_temperature_c,
-      #   #         "(°C)"
-      #   #       )
-      #   #     ) %>%
-      #   #     addLegend(
-      #   #       position = "bottomleft",
-      #   #       pal      = pal_tmp,
-      #   #       values   = dat_temps$sur_bin,
-      #   #       title    = "Surface </br> Temperature (°C)"
-      #   #     )
-      #   } 
-      # }else {
-      #   a
-      # }
-      
-      ## ADD STRATUM POLYGONS -----------------
+    
+      ## Survey Strata -----
       if (input$stratum) {
-        a <- 
-          a %>% 
+        leafletProxy(
+          "mymap"
+        ) %>%
+          clearGroup(
+            "survey_strata"
+          ) %>%
+          addMapPane(
+            "strata",
+            zIndex = 420
+          ) %>%
           addPolygons(
-            data = 
-              shp_all$survey.strata %>%
+            data      = shp_all$survey.strata %>%
               st_transform(crs = "+proj=longlat +datum=WGS84") %>%
               dplyr::filter(
                 SRVY %in% input$survey
@@ -374,9 +319,11 @@ s_surveymap <- function(id) {
               # better to not have the map render those polygons.
               dplyr::filter(
                 SRVY %!in% c("AI", "GOA")
-              ), 
-            weight    = 0.25,
-            color     = "black", 
+              ),
+            group     = "survey_strata",
+            options   = pathOptions(pane = "strata"),
+            weight    = 0.5,
+            color     = "black",
             fill      = "transparent",
             fillColor = "transparent",
             label     =  paste0(
@@ -386,37 +333,34 @@ s_surveymap <- function(id) {
                 SRVY %in% input$survey
               )$stratum
             ),
-            # Can toggle this on to have blank labels for those with NA stratum
-            # label     = ifelse(
-            #   !is.na(dplyr::filter(
-            #     shp_all$survey.strata, 
-            #     SRVY %in% input$survey
-            #   )$stratum),
-            #   paste0(
-            #   "Stratum: ", 
-            #   dplyr::filter(
-            #     shp_all$survey.strata, 
-            #     SRVY %in% input$survey
-            #   )$stratum
-            #   ),
-            #   ""
-            # ),
             highlightOptions = highlightOptions(
               fillColor    = 'grey50',
               fill         = 'grey50',
               bringToFront = TRUE
             )
           )
-      } else {
-        a
+      } else if (input$stratum == FALSE) {
+        leafletProxy(
+          "mymap"
+        ) %>%
+          clearGroup(
+            "survey_strata"
+          )
       }
-      
-      # ADD STATION POINTS ---------------------
-      if (input$station) {
-        
-        a <-
-          a %>%
-          addPolygons( 
+  
+      ## Stations -----
+      if (input$station && input$plot_unit == "none") {
+        leafletProxy(
+          "mymap"
+        ) %>%
+          clearGroup(
+              "survey_stations"
+          ) %>%
+          addMapPane(
+            "stations",
+            zIndex = 440
+          ) %>%
+          addPolygons(
               data = shp_all$survey.grid %>%
                 st_transform(crs = "+proj=longlat +datum=WGS84") %>%
                 dplyr::filter(
@@ -428,23 +372,21 @@ s_surveymap <- function(id) {
                 dplyr::filter(
                   SRVY %!in% c("AI", "GOA")
                 ),
+              group       = "survey_stations",
+              options     = pathOptions(pane = "stations"),
               weight      = 0.25,
               color       = "black",
               fillColor   = "black",
               label       = ~station,
-              # color   = nmfspalette::nmfs_palette(palette = "urchin")(1),
-              fillOpacity = 0.1,
-              # popup       = paste(
-              #   "<strong>Survey:</strong> ", df1$survey, "<br>",
-              #   "<strong>Data State:</strong> ", df1$data_type,  "<br>",
-              #   "<strong>Station:</strong> ", shp_stn$station, "<br>",
-              #   "<strong>Stratum:</strong> ", shp_stn$stratum,  "<br>",
-              #   "<strong>Latitude (&degN):</strong> ", round(shp_stn$lat, 2),  "<br>",
-              #   "<strong>Longitude (&degW):</strong> ", round(shp_stn$lon, 2),  "<br>"
-              # )
+              fillOpacity = 0.5,
           )
-      } else {
-        a
+      } else if (input$station == FALSE) {
+        leafletProxy(
+          "mymap"
+        ) %>%
+          clearGroup(
+            "survey_stations"
+          )
       }
     })
   })
