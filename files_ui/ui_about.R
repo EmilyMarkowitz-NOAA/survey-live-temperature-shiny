58bd60a4901e272f6805ddb5965791d1a648c3f3
MichaelSchram-NOAA-Affiliate <michael.schram@noaa.gov>
2024-07-29 11:24:28 -0700

    Update ui_about.R

--- files_ui/ui_about.R
+++ files_ui/ui_about.R
@@ -14,11 +14,11 @@ ui_about <- function() {
         h2("Resource Assessment and Conservation Engineering Division"),
         h3("Groundfish Assessment Program Bottom Trawl Survey"),
         
-        p("The RACE Groufish Assessment Program conducts bottom trawl surveys with the intent to collect data on the distribution and abundance of crab, groundfish, and other bottom-dwelling species in the Bering Sea, Aleutian Islands, and Gulf of Alaska. This work has been conducted annually since 1982 in the Eastern Bering sea. Bottom Trawl Surveys began more recently in other regions, and occur intermittently (i.e., Northern Bering Sea), or alternate biannually (i.e., Aleutian Islands and Gulf of Alaska)."), 
+        p("The RACE Groundfish Assessment Program conducts bottom trawl surveys with the intent to collect data on the distribution and abundance of crab, groundfish, and other bottom-dwelling species in the Bering Sea, Aleutian Islands, and Gulf of Alaska. This work has been conducted annually since 1982 in the Eastern Bering sea. Bottom trawl Surveys began more recently in other regions, and occur intermittently (i.e., Northern Bering Sea) or biannually in alternating years (i.e., Aleutian Islands and Gulf of Alaska)."), 
 
         br(),
 
-        p("This applications serves as a general-purpose tool to explore and visualize those data. For more specific needs or collaboration, please contact <First.Last@noaa.gov>")
+        p("This application serves as a general-purpose tool to explore and visualize those data. For more specific needs or collaboration, please contact <First.Last@noaa.gov>")
       ),
       # target the box header of About
       tags$head(tags$style('#About .box-header{ display: none}'))  
