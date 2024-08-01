069eff4a517438404eda9ff6b547d3947d1b3b0b
MichaelSchram-NOAA-Affiliate <michael.schram@noaa.gov>
2024-08-01 09:07:33 -0700

    Update ui_glossary.R
    Formatting consistency with other informational tabs

--- files_ui/ui_glossary.R
+++ files_ui/ui_glossary.R
@@ -4,15 +4,23 @@ ui_glossary <- function(id) {
   tabItem(
     tabName = "glossary",
     fluidRow(
-      HTML("<html lang='en'>"), #Always have this as your first line
-      
-      h1("Glossary of Terms"),
-      column(
-        12, 
-        wellPanel(
-          DT::dataTableOutput(ns("gloss"))
+      box(
+        width        = 12, 
+        id           = "glossary",
+        title        = NULL,
+        headerBorder = FALSE,
+        HTML("<html lang='en'>"), #Always have this as your first line
+        
+        h1("Glossary of Terms"),
+        column(
+          12, 
+          wellPanel(
+            DT::dataTableOutput(ns("gloss"))
+          )
         )
-      )
+      ),
+      # target the box header of About
+      tags$head(tags$style('#glossary .box-header{ display: none}'))  
     )
   )
 }
